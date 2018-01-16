e218effc766f58df73a78319f2ce05e586ee65fc
pavlovyuriy <yuriy.pavlov@castle.co>
2018-01-17 00:10:33 +0400

    wip

--- app/services/immediate_task_service.rb
+++ app/services/immediate_task_service.rb
@@ -1,51 +1,46 @@
 class ImmediateTaskService
   def self.call
-    t =
-        User.find_by_sql [%Q(
-          SELECT DISTINCT ON (user_id)
-            users.*,
-            tasks.id as task_id
-          FROM users
-          LEFT JOIN
-            (
-              SELECT
-                *,
-                1 as sort_type,
-                deadline as sort_value
-              FROM
-                tasks
-              WHERE
-                tasks.completed_at IS NULL AND tasks.deadline > :now
-              UNION
-              SELECT
-                *,
-                2 as sort_type,
-                deadline as sort_value
-              FROM
-                tasks
-              WHERE
-                tasks.completed_at IS NULL AND tasks.deadline <= :now
-              UNION
-              SELECT
-                *,
-                3 as sort_type,
-                created_at as sort_value
-              FROM
-                tasks
-              WHERE
-                tasks.completed_at IS NULL AND tasks.deadline IS NULL
-            ) as tasks
-          ON
-            users.id = tasks.user_id
-          ORDER BY
-            user_id ASC,
-            sort_type ASC,
-            sort_value ASC
-        ), { now: Time.zone.now }]
-
-    t.to_s
-
-    t
+      User.find_by_sql [%Q(
+        SELECT DISTINCT ON (user_id)
+          users.*,
+          tasks.id as task_id
+        FROM users
+        LEFT JOIN
+          (
+            SELECT
+              *,
+              1 as sort_type,
+              deadline as sort_value
+            FROM
+              tasks
+            WHERE
+              tasks.completed_at IS NULL AND tasks.deadline < :now
+            UNION
+            SELECT
+              *,
+              2 as sort_type,
+              deadline as sort_value
+            FROM
+              tasks
+            WHERE
+              tasks.completed_at IS NULL AND tasks.deadline >= :now
+            UNION
+            SELECT
+              *,
+              3 as sort_type,
+              created_at as sort_value
+            FROM
+              tasks
+            WHERE
+              tasks.completed_at IS NULL AND tasks.deadline IS NULL
+          ) as tasks
+        ON
+          users.id = tasks.user_id
+        ORDER BY
+          user_id ASC,
+          sort_type ASC,
+          sort_value ASC
+      ), { now: Time.zone.now }]
 
   end
 end
--- spec/services/immediate_task_service_spec.rb
+++ spec/services/immediate_task_service_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe 'ImmediateTaskService' do
 
     it {
       user = create(:user)
-      task_1 = create(:task, :expired, user: user)
-      task_2 = create(:task, :expired, user: user)
+      task_1 = create(:task, :expired, user: user, deadline: current_time - 10.days)
+      task_2 = create(:task, :expired, user: user, deadline: current_time - 5.days)
+      task_3 = create(:task, :expired, user: user, deadline: current_time - 2.days)
 
       result =
         Timecop.freeze(current_time) do
@@ -17,10 +18,104 @@ RSpec.describe 'ImmediateTaskService' do
 
       expect(result).to eq [user]
 
-      puts result.first['task_id']
-
-      # expect( )
+      expect(result.first['task_id']).to eq task_1.id
     }
+
+    it {
+      user = create(:user)
+      task_1 = create(:task, :not_expired, user: user, deadline: current_time + 10.days)
+      task_2 = create(:task, :not_expired, user: user, deadline: current_time + 5.days)
+      task_3 = create(:task, :not_expired, user: user, deadline: current_time + 2.days)
+
+      result =
+          Timecop.freeze(current_time) do
+            ImmediateTaskService.call
+          end
+
+      expect(result).to eq [user]
+
+      expect(result.first['task_id']).to eq task_3.id
+    }
+
+    it {
+      user = create(:user)
+      task_1 = create(:task, :without_deadline, user: user, deadline: nil, created_at: current_time - 1.days)
+      task_2 = create(:task, :without_deadline, user: user, deadline: nil, created_at: current_time - 10.days)
+      task_3 = create(:task, :without_deadline, user: user, deadline: nil, created_at: current_time - 3.days)
+
+      result =
+          Timecop.freeze(current_time) do
+            ImmediateTaskService.call
+          end
+
+      expect(result).to eq [user]
+
+      expect(result.first['task_id']).to eq task_2.id
+    }
+
+    it {
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+
+      task_1_1 = create(:task, :expired, user: user_1, deadline: current_time - 10.days)
+      task_1_2 = create(:task, :expired, user: user_2, deadline: current_time - 5.days)
+      task_1_3 = create(:task, :expired, user: user_3, deadline: current_time - 2.days)
+
+      task_2_1 = create(:task, :not_expired, user: user_1, deadline: current_time + 10.days)
+      task_2_2 = create(:task, :not_expired, user: user_2, deadline: current_time + 5.days)
+      task_2_3 = create(:task, :not_expired, user: user_3, deadline: current_time + 2.days)
+
+      task_3_1 = create(:task, :without_deadline, user: user_1, deadline: nil, created_at: current_time - 1.days)
+      task_3_2 = create(:task, :without_deadline, user: user_2, deadline: nil, created_at: current_time - 10.days)
+      task_3_3 = create(:task, :without_deadline, user: user_3, deadline: nil, created_at: current_time - 3.days)
+
+      result =
+          Timecop.freeze(current_time) do
+            ImmediateTaskService.call
+          end
+
+      puts User.all.to_a.inspect
+      result = result.sort_by(&:id)
+
+      expect(result).to eq [user_1, user_2, user_3]
+
+      expect(result.first['task_id']).to eq task_1_1.id
+      expect(result.second['task_id']).to eq task_1_2.id
+      expect(result.third['task_id']).to eq task_1_3.id
+    }
+
+
+    it {
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      user_4 = create(:user)
+
+      task_1_3 = create(:task, :expired, user: user_3, deadline: current_time - 2.days)
+
+      task_2_2 = create(:task, :not_expired, user: user_2, deadline: current_time + 5.days)
+      task_2_3 = create(:task, :not_expired, user: user_3, deadline: current_time + 2.days)
+
+      task_3_1 = create(:task, :without_deadline, user: user_1, deadline: nil, created_at: current_time - 1.days)
+      task_3_2 = create(:task, :without_deadline, user: user_2, deadline: nil, created_at: current_time - 10.days)
+      task_3_3 = create(:task, :without_deadline, user: user_3, deadline: nil, created_at: current_time - 3.days)
+
+      result =
+          Timecop.freeze(current_time) do
+            ImmediateTaskService.call
+          end
+
+      result = result.sort_by(&:id)
+
+      expect(result).to eq [user_1, user_2, user_3, user_4]
+
+      expect(result.first['task_id']).to eq task_3_1.id
+      expect(result.second['task_id']).to eq task_3_2.id
+      expect(result.third['task_id']).to eq task_3_3.id
+      expect(result[4]['task_id']).to be nil
+    }
+
   end
 end
 
