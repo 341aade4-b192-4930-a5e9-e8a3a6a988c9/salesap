e058387d032420f3e017d3664652eacebe771855
pavlovyuriy <yuriy.pavlov@castle.co>
2018-01-16 04:39:10 +0400

    added sql

--- app/services/immediate_task_service.rb
+++ app/services/immediate_task_service.rb
@@ -4,42 +4,33 @@ class ImmediateTaskService
         User.joins(%Q(
           LEFT JOIN
             (
-              SELECT DISTINCT ON (user_id)
-                *
+              SELECT
+                *,
+                1 as sort_type,
+                deadline as sort_value
               FROM
-                (
-                  SELECT
-                    *,
-                    1 as sort_type,
-                    deadline as sort_value
-                  FROM
-                    tasks
-                  WHERE
-                    tasks.completed_at IS NULL AND tasks.deadline > NOW()
-                  UNION
-                  SELECT
-                    *,
-                    2 as sort_type,
-                    deadline as sort_value
-                  FROM
-                    tasks
-                  WHERE
-                    tasks.completed_at IS NULL AND tasks.deadline <= NOW()
-                  UNION
-                  SELECT
-                    *,
-                    3 as sort_type,
-                    created_at as sort_value
-                  FROM
-                    tasks
-                  WHERE
-                    tasks.completed_at IS NULL AND tasks.deadline IS NULL
-                ) as marked_tasks
-              ORDER BY
-                user_id ASC,
-                sort_type ASC,
-                sort_value ASC
-            ) as ordered_tasks
+                tasks
+              WHERE
+                tasks.completed_at IS NULL AND tasks.deadline > NOW()
+              UNION
+              SELECT
+                *,
+                2 as sort_type,
+                deadline as sort_value
+              FROM
+                tasks
+              WHERE
+                tasks.completed_at IS NULL AND tasks.deadline <= NOW()
+              UNION
+              SELECT
+                *,
+                3 as sort_type,
+                created_at as sort_value
+              FROM
+                tasks
+              WHERE
+                tasks.completed_at IS NULL AND tasks.deadline IS NULL
+            ) as marked_tasks
           ON
             users.id = ordered_tasks.user_id
         )).all
